28bcfb5130d2748c0e9dbe9a8f0baed91b8dacd9
Bhavesh Achhada <bhavesh@agricarbon.co.uk>
2026-07-18 12:11:01 +0530

    feat: added QLearning agent

---
+++ packages/agents/src/agents/q_learning.py
@@ -0,0 +1,59 @@
+from typing import Union, Callable
+
+from packages.agents.src.agents.agent import Agent, ObsType, ActType
+from packages.environments.src.environments.environment import Environment
+from packages.environments.src.environments.space import DiscreteSpace
+from packages.policies.src.policies.policy import Policy
+
+
+type Number = Union[int, float]
+
+
+class QLearningAgent(Agent[ObsType, ActType]):
+    def __init__(
+        self,
+        env: Environment,
+        policy: Policy,
+        q_value_getter: Callable[[ObsType, ActType], Number],
+        q_value_setter: Callable[[ObsType, ActType, Number], None],
+        discount_factor: float,
+        learning_rate: float,
+    ):
+        self.env = env
+        self.policy = policy
+
+        assert isinstance(self.env.action_space, DiscreteSpace), (
+            f"{self.__class__.__name__} only supports DiscreteSpace for Actions"
+        )
+
+        self.get_q_value = q_value_getter
+        self.set_q_value = q_value_setter
+        self.discount_factor = discount_factor
+        self.learning_rate = learning_rate
+
+    def choose_action(self, state: ObsType) -> int:
+        return self.policy.choose_action(state)
+
+    def _max_q_for_state(self, state: ObsType) -> Number:
+        n = self.env.action_space.n
+        start = self.env.action_space.start
+        return max(self.get_q_value(state, a) for a in range(start, start + n))
+
+    def step(
+        self,
+        state: ObsType,
+        action: ActType,
+        reward: Number,
+        next_state: ObsType,
+        next_action: ActType = None,
+        done: bool = False,
+    ):
+        current_q = self.get_q_value(state, action)
+        target = (
+            reward
+            if done
+            else reward + (self.discount_factor * self._max_q_for_state(next_state))
+        )
+        error = target - current_q
+        updated_q = current_q + self.learning_rate * error
+        self.set_q_value(state, action, updated_q)
